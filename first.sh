959649076852d9a9a36510ae1384ceb87ac6c066
Per Mafrost <per@dohi.se>
2014-10-16 23:34:53 +0200

    a

--- first.sh
+++ first.sh
@@ -8,3 +8,6 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.17.2/install.sh | bash
 nvm install 0.10
 nvm use 0.10
 npm install -g grunt-cli
+
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
